d572ab492c173e0f532e8aa51016f63756624c36
lumifa <lmphat20@clc.fitus.edu.vn>
2023-03-30 02:16:52 +0700

    add HomePage without login
    (first test)

--- app/src/main/java/com/example/hyv_hpv_clinicbooking/HomePageGuest.kt
+++ app/src/main/java/com/example/hyv_hpv_clinicbooking/HomePageGuest.kt
@@ -3,10 +3,9 @@ package com.example.hyv_hpv_clinicbooking
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class HomePageGuest : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        setContentView(R.layout.activity_home_page_guest)
     }
 }
