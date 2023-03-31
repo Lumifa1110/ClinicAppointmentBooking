d98e97ee2adde9e92fc46e3438235149b3ca4e6b
ptminngoc <pngoc0105@gmail.com>
2023-03-31 12:39:19 +0700

    login

--- app/src/main/java/com/example/hyv_hpv_clinicbooking/MainActivity.kt
+++ app/src/main/java/com/example/hyv_hpv_clinicbooking/MainActivity.kt
@@ -6,8 +6,13 @@ import android.os.Bundle
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    val REQUEST_CODE = 1111;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val intent = Intent(this, LoginPage::class.java)
+        startActivityForResult(intent, REQUEST_CODE)
     }
 }
