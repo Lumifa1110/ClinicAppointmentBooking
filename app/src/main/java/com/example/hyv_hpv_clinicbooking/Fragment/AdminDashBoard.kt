ff55573a2c7f2de24227b243ad952bcb73c42fb9
Lumifa1110 <lmphat20@clc.fitus.edu.vn>
2023-04-06 01:39:35 +0700

    update Admin Page (Doctor Management)

--- app/src/main/java/com/example/hyv_hpv_clinicbooking/Fragment/AdminDashBoard.kt
+++ app/src/main/java/com/example/hyv_hpv_clinicbooking/Fragment/AdminDashBoard.kt
@@ -9,10 +9,6 @@ import com.example.hyv_hpv_clinicbooking.R
 
 
 class AdminDashBoard : Fragment() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
