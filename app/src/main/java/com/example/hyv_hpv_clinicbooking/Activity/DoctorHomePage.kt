3f88f0234f464582af9d825636104d2dcea468b3
ptminngoc <pngoc0105@gmail.com>
2023-04-15 02:17:17 +0700

    fix link

--- app/src/main/java/com/example/hyv_hpv_clinicbooking/Activity/DoctorHomePage.kt
+++ app/src/main/java/com/example/hyv_hpv_clinicbooking/Activity/DoctorHomePage.kt
@@ -2,6 +2,7 @@ package com.example.hyv_hpv_clinicbooking.Activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.view.get
 import com.example.hyv_hpv_clinicbooking.Fragment.*
 import com.example.hyv_hpv_clinicbooking.R
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -10,13 +11,23 @@ class DoctorHomePage : AppCompatActivity() {
     var bottomNavBar: BottomNavigationView? = null
     var doctorHomeFrament = DoctorHomeFragment()
     var appoinmentManagementFragment = AppoinmentManagementFragment()
+    var doctorArrangeDayFragment = DoctorChooseFreeTimeFragment()
+    var doctorProfileFragment = DoctorProfile()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_doctor_home_page)
         bottomNavBar = findViewById(R.id.doctorNavBar) as BottomNavigationView
-
-
         supportFragmentManager.beginTransaction().replace(R.id.container, doctorHomeFrament).commit()
+
+        val tab = intent.getStringExtra("fragment")
+        if(tab.equals("profile")) {
+            val function = intent.getStringExtra("function")
+            if(function == "edit") {
+                //Cap Nhat DB
+            }
+            supportFragmentManager.beginTransaction().replace(R.id.container, doctorProfileFragment).commit()
+            bottomNavBar?.menu!!.get(3).isChecked = true
+        }
 
 
         bottomNavBar!!.setOnNavigationItemSelectedListener {
@@ -30,9 +41,11 @@ class DoctorHomePage : AppCompatActivity() {
                     return@setOnNavigationItemSelectedListener true
                 }
                 R.id.doctorSchedule -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.container, doctorArrangeDayFragment).commit()
                     return@setOnNavigationItemSelectedListener true
                 }
                 R.id.doctorProfile -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.container, doctorProfileFragment).commit()
                     return@setOnNavigationItemSelectedListener true
                 }
                 else -> false
